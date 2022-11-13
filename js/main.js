882b961b58babe0d67a2df9ff2fd57553e7d6e3d
mshamrok <mshamrok@gmail.com>
2022-11-13 09:34:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+function showlist() {
+    var element = document.getElementById("list");
+    var arrowstat = document.getElementById("arrow");
+    element.classList.add("showlist");
+    arrowstat.classList.add("rotate");
+  }
